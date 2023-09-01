e36affbd723c9ef2110e5dd4ade40d8e62a86317
ark <felipe.leal@mail.uft.edu.br>
2023-09-01 09:42:16 -0300

    Update and rename 1.09.txt to 1.09.c

--- MyFlow/1.09.c
+++ MyFlow/1.09.c
@@ -5,28 +5,25 @@ float soma = 0, media;
 int idade, cont = 0;
 char resp;
 do {
+resp = 'k';
 printf("Digite a idade: ");
-scanf("%d", &idade);
+scanf(" %d", &idade);
 soma = soma + idade;
 cont = cont + 1;
 media = soma/cont;
 if(idade >= 18){
     printf("Maior de idade\n\n");
+    //printf("%c", resp);
 }
 else{
     printf("Menor de idade\n\n");
 }
 while(resp != 's' && resp != 'n' && resp != 'S' && resp != 'N'){
-    while((getchar())!='\n');
-    resp = 'k';
+while((getchar())!='\n');
+    
 printf("Quer continuar? Digite 's' para sim ou 'n' para nao: ");
 scanf(" %c", &resp);
 }
 } while(resp != 'n');
 printf("A media das idades e de %.2f\n", media);
-
-
-
-
-
 }
