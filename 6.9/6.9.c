154e66b315e585ab3a464a090085496cffa4f409
ark <felipe.leal@mail.uft.edu.br>
2023-09-06 12:10:56 +0000

    a

--- 6.9/6.9.c
+++ 6.9/6.9.c
@@ -4,11 +4,22 @@
 int main() {
     // Write C code here
     
-    int senha = 0;
+    int senha = 1;
+    char nome[50];
+
+    printf("nome: ");
+    scanf("%s", & nome);
+     while(strcmp(nome, "fim")){
+        printf("%s", nome);
+        printf("%d", senha);
+        senha++;
+        if(senha >= 50){
+            break;
+        }
+    printf("nome: ");
+    scanf("%s", & nome);
     
-
-
-
-
+    }
     return 0;
+    printf("fim");
 }
