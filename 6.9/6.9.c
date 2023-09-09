f2d2258258f850d5fdfe61e1aea285c7b7091640
ark <felipe.leal@mail.uft.edu.br>
2023-09-09 02:38:31 +0000

    edit

--- 6.9/6.9.c
+++ 6.9/6.9.c
@@ -6,13 +6,12 @@ char ask[100];
 
 float rFLOAT = 0;
 char answer[100];
-char rTipo;
-
-int InpFilial = 0;
+char rTipo[100];
 
 void clearIN(){
     rFLOAT = 0;
     strcpy(answer, "");
+    strcpy(rTipo, "");
 }
 void clearOUT(){
     while((getchar()) != '\n');
@@ -42,10 +41,10 @@ void readString(){
 
 void readTipo(){
     clearIN();
-    while (rTipo != 'm' && rTipo != '' && rTipo != 'C'){
+    while (rTipo != 'm' && rTipo != 'l' && rTipo != 'p' && rTipo != 'M' && rTipo != 'L' && rTipo != 'P'){
         printf("%s", ask);
         scanf(" %c", & rTipo);
-        if(rTipo != 'A' && rTipo != 'B' && rTipo != 'C'){
+        if(rTipo != 'm' && rTipo != 'l' && rTipo != 'p' && rTipo != 'M' && rTipo != 'L' && rTipo != 'P'){
             clearOUT();
         }
     }
@@ -67,29 +66,40 @@ Filial filiais[34];
 
 
 int main() {
+    int InpFilial;
     while(InpFilial > 34 || InpFilial < 1){
         strcpy(ask, "Filial (1-34): ");
         readFloat();
         InpFilial = rFLOAT;
     }
 
+    
     printf("----Filial:");
     printf("%d", InpFilial);
+    printf("----");
     printf("\n");
 
-    int ReadProd = 1;
-    bool ProdRead = true;
+    printf("deseja adicionar ou modificar um produto? (a/m): ");
+    int ReadProd = 1; 
+    bool ProdRead = true
+
     while(ProdRead){
+
         strcpy(ask, "nome do produto: ");
         readString();
         strcpy(filiais[InpFilial].produtos[ReadProd].nome, answer);
+
         strcpy(ask, "preço do produto: ");
         readFloat();
         filiais[InpFilial].produtos[ReadProd].preço = rFLOAT;
+
         strcpy(ask, "quantidade do produto: ");
         readFloat();
         filiais[InpFilial].produtos[ReadProd].quantidade = rFLOAT;
+
         strcpy(ask, "tipo do produto: ");
+        readTipo();
+        filiais[InpFilial].produtos[ReadProd].tipo = rTipo;
 
 
 
