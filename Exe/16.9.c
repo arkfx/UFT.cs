270b1d5ee324633d2da0f35ad82ae92401f0bf96
ark <felipe.leal@mail.uft.edu.br>
2023-09-15 13:50:46 +0000

    edit

--- Exe/16.9.c
+++ Exe/16.9.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-const int n = 10;
+#define N 10
 
 typedef struct{
     float idade;
 } p;
-p pessoas[n];
+p pessoas[N];
 
 int main() {
     int somaDaIdade = 0;
@@ -15,7 +15,7 @@ int main() {
     float PorcentagemComIdadeMenorQue18 = 0;
     int IdadeDaPessoaMaisVelha = 0;
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < N; i++){
         printf("Digite a idade da pessoa %d: ", i + 1);
         scanf("%f", &pessoas[i].idade);
         somaDaIdade += pessoas[i].idade;
@@ -26,9 +26,9 @@ int main() {
             PorcentagemComIdadeMenorQue18++;
         }
     }
-    media = somaDaIdade / n;
-    PorcentagemComIdadeMenorQue18 = (PorcentagemComIdadeMenorQue18 / n) * 100;
-    for(int i = 0; i < n; i++){
+    media = somaDaIdade / N;
+    PorcentagemComIdadeMenorQue18 = (PorcentagemComIdadeMenorQue18 / N) * 100;
+    for(int i = 0; i < N; i++){
         if (pessoas[i].idade < media){
             PessoasComIdadeMenorQueAMedia++;
         }
