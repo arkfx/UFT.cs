f12ee561232f56b3ef605eb00b9d6e2e1beca782
ark <felipe.leal@mail.uft.edu.br>
2023-09-16 12:48:58 +0000

    e

--- 2023.2/15.9.c
+++ 2023.2/15.9.c
@@ -1,11 +1,15 @@
-// Online C compiler to run C program online
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+
+
 
 int main() {
-    // Write C code here
     printf("Hello world");
-    int a = 1;
-    int a, c[3];
+
 
     return 0;
 }
