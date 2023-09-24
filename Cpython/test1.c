8878f071f476817b411b25806cc97b2e80abbf00
ark <felipe.leal@mail.uft.edu.br>
2023-09-24 20:17:05 +0000

    a

--- Cpython/test1.c
+++ Cpython/test1.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    char input[100]; // Declara um array de caracteres para armazenar a entrada do usuário
+    char text[1000];
 
-    printf("Digite um valor: ");
-    scanf("%s", input); // Lê a entrada do usuário e armazena em 'input'
+    printf("Enter the text to save: ");
+    scanf("%s", text);
 
-    // Imprime a entrada no arquivo 'output.txt' na mesma pasta do programa em C
-    FILE *arquivo;
-    arquivo = fopen("output.txt", "w");
-    fprintf(arquivo, "%s", input);
-    fclose(arquivo);
-    
+    FILE *file = fopen("output.txt", "w");
+    fprintf(file, "%s", text);
+    fclose(file);
+
     return 0;
 }
--- Cpython/test2.py
+++
@@ -1,3 +0,0 @@
-with open('output.txt', 'r') as file:
-    contents = file.read()
-    print(contents)
