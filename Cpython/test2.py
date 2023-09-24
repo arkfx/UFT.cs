10a70eeadb77e44084db3fa697ccc77acdee20b6
ark <felipe.leal@mail.uft.edu.br>
2023-09-24 20:02:59 +0000

    a

--- Cpython/test2.py
+++ Cpython/test2.py
@@ -1,6 +1,2 @@
-# Abre o arquivo 'output.txt' gerado pelo programa em C
-with open('output.txt', 'r') as arquivo:
-    valor = arquivo.read()
-
-# Imprime o valor lido do arquivo
-print("O valor lido do arquivo é:", valor)
+import os
+print(os.getcwd())
