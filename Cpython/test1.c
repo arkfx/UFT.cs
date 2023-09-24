2e650807dcfbef869f714f645cfbda048357ffae
ark <felipe.leal@mail.uft.edu.br>
2023-09-24 19:51:24 +0000

    a

--- Cpython/test1.c
+++ Cpython/test1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     char input[100]; // Declara um array de caracteres para armazenar a entrada do usuário
@@ -6,7 +7,7 @@ int main() {
     printf("Digite um valor: ");
     scanf("%s", input); // Lê a entrada do usuário e armazena em 'input'
 
-    // Imprime a entrada em um arquivo (output.txt)
+    // Imprime a entrada no arquivo 'output.txt' na mesma pasta do programa em C
     FILE *arquivo;
     arquivo = fopen("output.txt", "w");
     fprintf(arquivo, "%s", input);
