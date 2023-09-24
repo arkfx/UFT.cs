3c1c66a9b4ee514d38b022fab7f8852d69ba31d2
ark <felipe.leal@mail.uft.edu.br>
2023-09-24 21:30:22 +0000

    z

--- Cpython/frontPY/test2.py
+++ Cpython/frontPY/test2.py
@@ -1,3 +1,7 @@
+"""
+This module reads the contents of a file named 'output.txt' and prints it to the console.
+"""
+
 #start
 
 with open("/workspaces/UFT.cs/Cpython/backC/output.txt", "r", encoding="utf-8") as file:
