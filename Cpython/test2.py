6f8c28cb805facea2774c5e690a245776287e6a6
ark <felipe.leal@mail.uft.edu.br>
2023-09-24 20:27:38 +0000

    w

--- Cpython/test2.py
+++ Cpython/test2.py
@@ -1,3 +1,7 @@
-with open('output.txt', 'r') as file:
+"""
+This module reads a file called output.txt and prints its contents to the console.
+"""
+
+with open('output.txt', 'r', encoding='utf-8') as file:
     contents = file.read()
     print(contents)
