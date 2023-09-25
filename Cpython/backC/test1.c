4bd4259091fc569d002a29426f6230c3f65b5bb9
ark <felipe.leal@mail.uft.edu.br>
2023-09-25 20:58:10 +0000

    a

--- Cpython/backC/test1.c
+++ Cpython/backC/test1.c
@@ -4,14 +4,5 @@
 #include <unistd.h>
 
 int main(){
-    FILE *fp;
-    char input[100];
-
-    fp = fopen("output.txt", "w");
-    printf("Enter input: ");
-    scanf("%s", input);
-
-    fprintf(fp, "%s", input);
-    printf("Input saved to output.txt\n");
-    return 0;
+    
 }
