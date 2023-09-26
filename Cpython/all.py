1fcd321e2d4d9ce02a777b11cdb7e9268f2009e7
ark <felipe.leal@mail.uft.edu.br>
2023-09-26 22:57:46 +0000

    A

---
+++ Cpython/all.py
@@ -0,0 +1,19 @@
+"""
+This module runs two scripts at the same time.
+
+The first script is located at /workspaces/UFT.cs/Cpython/backC/test1.c and is compiled using gcc.
+The second script is located at /workspaces/UFT.cs/Cpython/frontPY/test2.py and is run using Python.
+
+This module uses the subprocess module to start the two scripts in separate processes.
+"""
+import subprocess
+
+# Rest of the code goes here
+
+
+# Run test1.c in the background
+subprocess.Popen(['./test1'])
+
+# Run test2.py in the background
+subprocess.Popen(['python', '/workspaces/UFT.cs/Cpython/frontPY/test2.py'])
+
