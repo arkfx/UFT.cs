e250168aa41fd538616adaba8f71bbb06dcb11a4
ark <felipe.leal@mail.uft.edu.br>
2023-10-24 19:20:12 +0000

    a

---
+++ Exe/24.10.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/* Exercício de CRUD em C: Gerenciamento de Lista de Contatos
+Crie um programa em C para gerenciar uma lista de contatos.
+O programa deve permitir que o usuário realize as seguintes operações:
+
+Create (Criar): O usuário deve poder adicionar um novo contato à lista. 
+Cada contato deve incluir um nome e um número de telefone. 
+Os contatos devem ser armazenados em uma estrutura de dados, como uma matriz.
+
+Read (Ler): O usuário deve poder listar todos os contatos na lista, 
+exibindo seus nomes e números de telefone.
+
+Update (Atualizar): O usuário deve poder atualizar o 
+número de telefone de um contato existente. 
+Isso deve ser feito selecionando um contato da lista pelo nome
+e atualizando seu número de telefone.
+
+Delete (Excluir): O usuário deve poder excluir um contato da lista, 
+selecionando-o pelo nome.
+
+Lembre-se de fornecer um menu de opções ao usuário e garantir que o programa 
+possa lidar com múltiplos contatos na lista. */
+
+
+typedef struct {
+    char name[50];
+    int number;
+} contact;
+contact contacts[1000];
+
+int main() {    
+    printf("\n");
+    printf("1 = Criar Contato \n 2 = Ler contato \n 3 = Atualizar Contato \n 4 = Deletar Contato");
+    int resp;
+    scanf("%d", & resp);
+    switch (expression)
+    {
+
+
+
+
+    return 0;
+}
