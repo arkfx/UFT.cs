439f531b251f5c90a05090fccc616bb0712c4f7f
ark <felipe.leal@mail.uft.edu.br>
2023-10-25 11:47:37 +0000

    a

---
+++ Exe/25.10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double num = 16.0;
+    double result = sqrt(num);
+    printf("The square root of %lf is %lf\n", num, result);
+    return 0;
+}
