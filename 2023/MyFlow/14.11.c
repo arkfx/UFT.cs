b537767e0e16c0bc9413468dde53ba1ec6634362
ark <felipe.leal@mail.uft.edu.br>
2023-11-16 00:34:50 +0000

    A

---
+++ 2023/MyFlow/14.11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//binary tree
+//insert
+//search
+//delete
+//modify
+//print
+//print in order
+//print in level
+
+
+int main() {
+    printf("Hello World\n");
+
+
+    return 0;
+
+
+}
+    
