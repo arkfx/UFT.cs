e0265d207c09e573bbd27d8591f90fbd0b0cc4c9
ark <felipe.leal@mail.uft.edu.br>
2024-01-17 04:58:32 +0000

    a

--- samples/demo/src/main/java/com/example/App.java
+++ samples/demo/src/main/java/com/example/App.java
@@ -14,12 +14,16 @@ import java.util.Scanner;
     com mesma cor em linhas diferentes.
 
     Após esta leitura inicial, ofereça ao vendedor, sempre que desejar, a possibilidade de consultar um
-    destes relatórios: Relatório geral (mostre todos os veículos e seus dados, a quantidade total de
-    veículos e a percentagem de ocupação das garagens), relatório por cor (dada a cor, mostre a
-    quantidade total e todos os veículos desta cor), relatório por código (dado o código, mostre a
-    quantidade total e todos os veículos com o código e seus dados), busca por veículo (Dados o
-    código do veículo e sua cor, mostre a quantidade. Se o veículo for encontrado, a busca deve ser
-    interrompida). Pode ocorrer a necessidade de cadastrar um veículo que tenha sido adquirido
+    destes relatórios: 
+    Relatório geral (mostre todos os veículos e seus dados, a quantidade total de
+    veículos e a percentagem de ocupação das garagens), 
+    relatório por cor (dada a cor, mostre a
+    quantidade total e todos os veículos desta cor), 
+    relatório por código (dado o código, mostre a
+    quantidade total e todos os veículos com o código e seus dados), 
+    busca por veículo (Dados o código do veículo e sua cor, mostre a quantidade. 
+    Se o veículo for encontrado, a busca deve ser interrompida). 
+    Pode ocorrer a necessidade de cadastrar um veículo que tenha sido adquirido
     esporadicamente, então o programa deve ser capaz de inserir um veículo (apenas 1) a qualquer
     momento.  */
 
@@ -28,16 +32,22 @@ public class App
     public static void main(String[] args)
     {
         //System.out.println( "Hello World!" );
-        
+        menu();
     
     }
 
     //main menu
     public static void menu() {
-        System.out.println("1 - Relatórios");   
-        System.out.println("2 - Inserir veículo");
-        System.out.println("3 - Remover veículo");
-        System.out.println("3 - Sair");
+        System.out.println("1 - Inserir veículo");
+        System.out.println("2 - Remover veículo");
+        System.out.println("3 - Relatórios");   
+        System.out.println("4 - Sair");
+
+        Scanner input = new Scanner(System.in);
+        int resp = input.nextInt();
+        
+
+        input.close();
     }
 
     public static void reports() {
