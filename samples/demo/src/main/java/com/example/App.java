f9909b758e2a7338ccb7753ab0d0564b0b075097
ark <felipe.leal@mail.uft.edu.br>
2024-01-17 04:16:10 +0000

    a

---
+++ samples/demo/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package samples.demo.src.main.java.com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
