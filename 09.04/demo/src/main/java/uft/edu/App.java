75fbabf0aab8a72b2673ff9336203d1d56c1b615
ark <felipe.leal@mail.uft.edu.br>
2024-04-16 17:12:33 +0000

    a

---
+++ 09.04/demo/src/main/java/uft/edu/App.java
@@ -0,0 +1,15 @@
+package uft.edu;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
