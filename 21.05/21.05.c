63a5c3db6e4add114ad34d890f3950f1e68fdcdf
ark <felipe.leal@mail.uft.edu.br>
2024-05-21 12:09:43 +0000

    chore: Add stack.h to file associations in VSCode settings.json

--- 21.05/21.05.c
+++ 21.05/21.05.c
@@ -5,7 +5,26 @@ int main() {
     printf("Dynamic Pile");
 
     printf("--interface--");
-    printf("' - Pile creation \n 2 - push \n 3 - pop \n 4 - read top \n 5 - pile delete \n 6 - print data");
+    printf("1 - Pile creation \n 2 - push \n 3 - pop \n 4 - read top \n 5 - pile delete \n 6 - print data");
+    int op;
+    scanf("%d", &op);
+    switch 
     
     return 0;
+}
+
+Stack_create(unsigned int size) {
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
+    stack->size = size;
+    stack->top = -1;
+    stack->data = (int *)malloc(size * sizeof(int));
+    return stack;
+}
+
+bool Stack_push(Stack *stack, int value) {
+    if (Stack_is_full(stack)) {
+        return false;
+    }
+    stack->data[++stack->top] = value;
+    return true;
 }
